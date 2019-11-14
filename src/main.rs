cb63c93c9d9b248418c9534eec7dbfc24c66315e
Christian Poulsen <cp@blackbird.online>
2019-11-14 15:56:51 +0900

    Added a busy loop to simulate cpu high usage

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     if args[1] == "server" {
-        server::run();
+        let mut seconds = 5;
+        let arg2 = args.get(2);
+        if arg2 != None {
+            seconds = match args[2].parse::<u64>() {
+                Ok(i) => i,
+                Err(_e) => {
+                    seconds
+                }
+            };
+        }
+        server::run(seconds);
     } else if args[1] == "client" {
         client::run();
     } else {
--- src/server.rs
+++ src/server.rs
@@ -1,24 +1,55 @@
-use std::thread;
+use std::{thread, time};
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 
-pub fn handle_client(mut stream: TcpStream) {
+pub fn sleep(sec: u64) {
+    let dur = time::Duration::from_secs(sec);
+
+    println!("Sleep for {} seconds", dur.as_secs());
+    thread::sleep(dur);
+}
+
+pub fn busy_loop(sec: u64) {
+    let dur = time::Duration::from_secs(sec);
+
+    let now = time::Instant::now();
+    let mut earlier = now.elapsed();
+
+    println!("Busy for {} seconds", sec);
+
+    while now.elapsed() < dur {
+        if earlier.as_secs() < now.elapsed().as_secs() {
+            if now.elapsed().as_secs() != (sec-1) {
+                println!("Busy for {} more seconds ...", (sec-1)-earlier.as_secs());
+            } else if now.elapsed().as_secs() == (sec-1)  {
+                println!("Busy for {} more second ...", (sec-1)-earlier.as_secs());
+            }
+            earlier = now.elapsed();
+        }
+    }
+}
+
+
+pub fn handle_client(mut stream: TcpStream, dur_secs: u64) {
+    busy_loop(dur_secs);
+    sleep(dur_secs);
+    busy_loop(dur_secs);
     let mut data = [0 as u8; 50]; // using 50 byte buffer
     while match stream.read(&mut data) {
         Ok(size) => {
             // echo everything!
             stream.write(&data[0..size]).unwrap();
-            true
-        },
+            if size == 0 { false } else { true }
+    },
         Err(_) => {
             println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
             stream.shutdown(Shutdown::Both).unwrap();
             false
         }
-    } {}
+    } { }
 }
 
-pub fn run() {
+pub fn run(dur_secs: u64) {
     println!("Starting server...");
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
     // accept connections and process them, spawning a new thread for each one
@@ -26,11 +57,15 @@ pub fn run() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let address = stream.peer_addr().unwrap();
                 println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
+                let handle = thread::spawn(move|| {
                     // connection succeeded
-                    handle_client(stream)
+                    handle_client(stream, dur_secs)
                 });
+
+                handle.join().unwrap();
+                println!("End connection with: {}", address);
             }
             Err(e) => {
                 println!("Error: {}", e);
